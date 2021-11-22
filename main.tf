aa1293aa204b9e9fabafe8880ab1955d8fc7ba05
venkadali <47103638+venkadali@users.noreply.github.com>
2021-11-22 13:12:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 #creating ec2 instance
-resource "aws_instance" "myinstance" {
-  ami = "ami-090f920d1b48d8c81"
-  instance_type = "t2.micro"
+resource "aws_instance" "sample" {
+ami = "ami-090f920d1b48d8c81"
+instance_type = "t2.micro"
 }
